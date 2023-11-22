235edab64d5bbd055fd1befd70a0555e9f723e72
oli.schatzberger <oliver.schatzberger@outlook.com>
2023-11-23 00:08:53 +0100

    added the checkbox selection for deleting entrys

--- app/frontend/src/ts/companyactions.ts
+++ app/frontend/src/ts/companyactions.ts
@@ -20,6 +20,11 @@ export async function deleteEntry(companyid: number) : Promise<any> {
 
 import axios from "axios"
 
+let myArray1: Array<HTMLElement> = [];
+
+let myArray2: Array<number> = [];
+
+
 export async function deleteEntry(companyId: number, element: HTMLElement): Promise<void> {
     const config = {
         headers: {
@@ -36,3 +41,39 @@ export async function deleteEntry(companyId: number, element: HTMLElement): Prom
         throw error;
     }
 }
+
+export async function deleteAllEntries(): Promise<void> {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+
+    try {
+        for (let i = 0; i < myArray1.length; i++) {
+            const response = await axios.delete(`http://localhost:4200/api/deleteCompany/${myArray2[i]}/company`, config);
+            console.log(response.data);
+            myArray1[i].parentElement?.parentElement?.remove();
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+
+}
+
+
+export function collectEntry(companyId: number, element: HTMLElement) {
+    console.log(companyId)
+    console.log(element)
+
+
+    myArray1.push(element);
+    myArray2.push(companyId);
+
+
+}
+
+
+
--- app/frontend/src/ts/render-table.ts
+++ app/frontend/src/ts/render-table.ts
@@ -1,5 +1,5 @@
 import { Company } from "Model/model";
-import {deleteEntry} from "./companyactions";
+import {collectEntry, deleteAllEntries, deleteEntry} from "./companyactions";
 
 export function render(companies: Company[]) {
     const tbody = document.querySelector("tbody");
@@ -8,11 +8,20 @@ export function render(companies: Company[]) {
     companies.map((company) => {
         const tr = document.createElement("tr");
         tbody.appendChild(tr);
-        tr.innerHTML = `<td class="py-6 px-6">${company.name}</td><td class="py-6 px-6">${company.address}</td><td class="py-6 px-6">${company.website}</td><td class="py-6 px-6"><button id="${company.id}edit" class="button primary-button">Edit</button><button id="${company.id}delete" class="button danger-button">Delete</button><input type="checkbox"></td>`;
-        document.getElementById(`${company.id}delete`).addEventListener("click", () => {
-            deleteEntry(company.id, document.getElementById(`${company.id}delete`))
+        tr.innerHTML = `<td class="py-6 px-6">${company.name}</td><td class="py-6 px-6">${company.address}</td><td class="py-6 px-6">${company.website}</td><td class="py-6 px-6"><input id="${company.id}" type="checkbox"></td>`;
+        document.getElementById(`${company.id}`).addEventListener("click", () => {
+            collectEntry(company.id, document.getElementById(`${company.id}`))
+            // deleteEntry(company.id, document.getElementById(`${company.id}`))
         });
     });
 }
+
+    const deletebutton = document.getElementById("deletebutton")
+
+    deletebutton.addEventListener("click", () => {
+        deleteAllEntries();
+    })
+
+
 
 
